68ccac3ac3d6716753eb8b96bda9c587ce43b05e
Pratham Bhargava <bhargavapratham1@gmail.com>
2024-02-12 16:22:22 +0530

    Tried @Query Native Query

--- src/main/java/com/example/LearningPortal/Repo/CourseRepository.java
+++ src/main/java/com/example/LearningPortal/Repo/CourseRepository.java
@@ -1,9 +1,15 @@
 package com.example.LearningPortal.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.LearningPortal.entity.Course;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+	@Query(value = "SELECT c FROM Course c where c.title = 'java'")
+	List<Course> findAllByTitleJavaCourses();
 
 }
